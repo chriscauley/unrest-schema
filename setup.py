63dd1ccd118dd4ca236ea15df1280afa514462e2
chriscauley <chris@lablackey.com>
2020-07-21 00:11:24 +0000

    README and renaming

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup, find_packages
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), 'r') as f:
+    long_description = f.read()
 
 setup(
   name = 'unrest_schema',
   packages = find_packages(),
-  version = '0.1',
-  description = '',
-  long_description="",
+  version = '0.1.1',
+  description = 'Convert Django forms into json schema',
+  long_description=long_description,
   long_description_content_type="text/markdown",
   author = 'Chris Cauley',
   author_email = 'chris@lablackey.com',
